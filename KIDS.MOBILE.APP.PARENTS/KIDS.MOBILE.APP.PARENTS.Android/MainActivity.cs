0cb667fc09106f9b2736db938fdfa5353eedb2b8
Kiara <Kiara@farental.com.au>
2020-11-18 20:20:37 +0700

    Splash screen

--- KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS.Android/MainActivity.cs
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Lottie.Forms.Droid;
 
 namespace KIDS.MOBILE.APP.PARENTS.Droid
 {
-    [Activity(Label = "HK Parent", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "HK Parent", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS.Android/SplashActivity.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+
+namespace KIDS.MOBILE.APP.PARENTS.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+        async void SimulateStartup()
+        {
+            await Task.Delay(8000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
