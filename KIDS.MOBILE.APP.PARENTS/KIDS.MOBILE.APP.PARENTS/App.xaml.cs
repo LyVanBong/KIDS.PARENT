4d0dc15b190c3ce12faefebeb14bbd14226fe152
kirapham <quanghuypham.95@gmail.com>
2020-11-20 23:20:05 +0700

    Tuition Fee

--- KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/App.xaml.cs
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/App.xaml.cs
@@ -10,6 +10,7 @@ using KIDS.MOBILE.APP.PARENTS.ViewModels.Home;
 using KIDS.MOBILE.APP.PARENTS.ViewModels.Login;
 using KIDS.MOBILE.APP.PARENTS.ViewModels.Main;
 using KIDS.MOBILE.APP.PARENTS.ViewModels.Notification;
+using KIDS.MOBILE.APP.PARENTS.ViewModels.Tuition;
 using KIDS.MOBILE.APP.PARENTS.ViewModels.User;
 using KIDS.MOBILE.APP.PARENTS.Views;
 using KIDS.MOBILE.APP.PARENTS.Views.Account;
@@ -17,6 +18,7 @@ using KIDS.MOBILE.APP.PARENTS.Views.Home;
 using KIDS.MOBILE.APP.PARENTS.Views.Login;
 using KIDS.MOBILE.APP.PARENTS.Views.Main;
 using KIDS.MOBILE.APP.PARENTS.Views.Notification;
+using KIDS.MOBILE.APP.PARENTS.Views.Tuition;
 using KIDS.MOBILE.APP.PARENTS.Views.User;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
@@ -54,6 +56,7 @@ namespace KIDS.MOBILE.APP.PARENTS
             containerRegistry.RegisterForNavigation<LoginPage, LoginViewModel>();
             containerRegistry.RegisterForNavigation<HomePage, HomeViewModel>();
             containerRegistry.RegisterForNavigation<NewsPage, NewsViewModel>();
+            containerRegistry.RegisterForNavigation<TuitionPage, TuitionViewModel>();
             #endregion
             #region Registry Dialog
             #endregion
@@ -63,7 +66,7 @@ namespace KIDS.MOBILE.APP.PARENTS
         {
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(AppSettings.SyncfusionLicense);
             InitializeComponent();
-            NavigationService.NavigateAsync(nameof(LoginPage));
+            NavigationService.NavigateAsync(nameof(TuitionPage));
         }
         protected override void OnStart()
         {
