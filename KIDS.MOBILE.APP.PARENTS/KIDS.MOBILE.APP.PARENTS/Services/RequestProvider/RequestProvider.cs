64201c2fee31eada0c0a77c9ff8b7c4ca8920e65
Kiara <Kiara@farental.com.au>
2020-12-16 20:31:53 +0700

    Post files to server

--- KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/Services/RequestProvider/RequestProvider.cs
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/Services/RequestProvider/RequestProvider.cs
@@ -79,6 +79,61 @@ namespace KIDS.MOBILE.APP.PARENTS.Services.RequestProvider
             }
         }
 
+        public async Task<ResponseModel<T>> PostAsync<T>(string uri, IReadOnlyCollection<RequestParameter> parameters, Dictionary<string, string> files = null)
+        {
+            try
+            {
+                //CreateClients(uri, Method.POST);
+                //if (parameters != null && parameters.Any())
+                //{
+                //    foreach (var item in parameters)
+                //    {
+                //        _request.AddParameter(item.Key, item.Value);
+                //    }
+                //}
+                //if (files?.Any() == true)
+                //{
+                //    foreach (var file in files)
+                //    {
+                //        _request.AddFile(file.Key, file.Value);
+                //    }
+                //}
+                //_request.AlwaysMultipartFormData = true;
+                //var response = await _client.ExecuteAsync<ResponseModel<T>>(_request);
+                //var data = response.StatusCode == HttpStatusCode.OK
+                //    ? JsonConvert.DeserializeObject<ResponseModel<T>>(response.Content)
+                //    : default;
+                //return data;
+                var url = AppConstants.UrlApiApp + uri;
+                RestClient client = new RestClient(url);
+                var request = new RestRequest(string.Empty, Method.POST);
+                //if (parameters != null && parameters.Any())
+                //{
+                //    foreach (var item in parameters)
+                //    {
+                //        request.AddParameter(item.Key,item.Value);
+                //    }
+                //}
+                request.AddJsonBody(JsonConvert.SerializeObject(parameters));
+                if (files?.Any() == true)
+                {
+                    foreach (var file in files)
+                    {
+                        request.AddFile(file.Key, file.Value);
+                    }
+                }
+                
+                request.AlwaysMultipartFormData = true;
+                request.AddHeader("Content-Type", "multipart/form-data");
+                IRestResponse response = client.Execute(request);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
         public async Task<ResponseModel<T>> PutAsync<T>(string uri, IReadOnlyCollection<RequestParameter> parameters)
         {
             try
