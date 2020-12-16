1f73be33b41a1144ed3b2d9ef7da93700b4b6f40
kirapham <quanghuypham.95@gmail.com>
2020-12-16 17:10:12 +0700

    Pickup Page

---
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/ViewModels/Pickup/PickupViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using KIDS.MOBILE.APP.PARENTS.Services;
+using Prism.Navigation;
+
+namespace KIDS.MOBILE.APP.PARENTS.ViewModels.Pickup
+{
+    public class PickupViewModel : BaseViewModel
+    {
+        private ObservableCollection<MessageModel> _messagesList = new ObservableCollection<MessageModel>();
+        public ObservableCollection<MessageModel> MessagesList
+        {
+            get => _messagesList;
+            set => SetProperty(ref _messagesList, value);
+        }
+        private ILeaveRequestService _leaveRequestService;
+
+        public PickupViewModel(INavigationService navigationService, ILeaveRequestService leaveRequestService) : base(navigationService)
+        {
+            _navigationService = navigationService;
+            _leaveRequestService = leaveRequestService;
+        }
+    }
+}
