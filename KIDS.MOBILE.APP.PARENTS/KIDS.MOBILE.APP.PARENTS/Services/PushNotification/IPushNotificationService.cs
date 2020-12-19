2e371b4e46a4ce99f83b54e46b37889190f62cbb
BongLV <hopehappyday@outlook.com>
2020-12-19 15:58:40 +0700

    Thêm Service thông báo đẩy

---
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/Services/PushNotification/IPushNotificationService.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using KIDS.MOBILE.APP.PARENTS.Models.Response;
+
+namespace KIDS.MOBILE.APP.PARENTS.Services.PushNotification
+{
+    public interface IPushNotificationService
+    {
+        /// <summary>
+        /// Lay id thiet bi
+        /// </summary>
+        /// <param name="groupId">
+        /// 0 la select theo user, 1 class, 2 school, 3 all
+        /// </param>
+        /// <param name="id">id tương ứng mới group id ở trên</param>
+        /// <returns></returns>
+        Task<ResponseModel<string>> GetDeviceId(string groupId, string id);
+
+        /// <summary>
+        /// Cập nhật id thiết bị cho user
+        /// </summary>
+        /// <param name="idSchool"></param>
+        /// <param name="idClass"></param>
+        /// <param name="idUser"></param>
+        /// <param name="idDevice"></param>
+        /// <param name="note"></param>
+        /// <returns></returns>
+        Task<ResponseModel<string>> UpdateDeviceDd(string idSchool, string idClass, string idUser, string idDevice, string note);
+        /// <summary>
+        /// Gửi thông báo
+        /// </summary>
+        /// <param name="idDevice"></param>
+        /// <param name="contentVi"></param>
+        /// <param name="contentEn"></param>
+        /// <param name="data1"></param>
+        /// <param name="data2"></param>
+        /// <param name="titleVi"></param>
+        /// <param name="titleEn"></param>
+        /// <returns></returns>
+        Task<ResponseModel<string>> SendNotifications(string idDevice, string contentVi, string contentEn, string data1, string data2, string titleVi, string titleEn);
+    }
+}
---
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/Services/PushNotification/PushNotificationService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using KIDS.MOBILE.APP.PARENTS.Models.RequestProvider;
+using KIDS.MOBILE.APP.PARENTS.Models.Response;
+using KIDS.MOBILE.APP.PARENTS.Services.RequestProvider;
+
+namespace KIDS.MOBILE.APP.PARENTS.Services.PushNotification
+{
+    public class PushNotificationService : IPushNotificationService
+    {
+        private IRequestProvider _requestProvider;
+        public PushNotificationService(IRequestProvider requestProvider)
+        {
+            _requestProvider = requestProvider;
+        }
+
+        public async Task<ResponseModel<string>> GetDeviceId(string groupId, string id)
+        {
+            try
+            {
+                var para = new List<RequestParameter>
+                {
+                    new RequestParameter("",groupId),
+                    new RequestParameter("",id),
+                };
+                var data = await _requestProvider.PostAsync<string>("PushNotifications/Select", para);
+                return data;
+            }
+            catch (Exception )
+            {
+                throw;
+            }
+        }
+
+        public async Task<ResponseModel<string>> UpdateDeviceDd(string idSchool, string idClass, string idUser, string idDevice, string note)
+        {
+            try
+            {
+                var para = new List<RequestParameter>
+                {
+                    new RequestParameter("IdSchool",idSchool),
+                    new RequestParameter("IdClass",idClass),
+                    new RequestParameter("IdUser",idUser),
+                    new RequestParameter("IdDevice",idDevice),
+                    new RequestParameter("Note",note),
+                };
+                var data = await _requestProvider.PostAsync<string>("PushNotifications/Update", para);
+                return data;
+            }
+            catch (Exception )
+            {
+                throw;
+            }
+        }
+
+        public async Task<ResponseModel<string>> SendNotifications(string idDevice, string contentVi, string contentEn, string data1, string data2, string titleVi,
+            string titleEn)
+        {
+            try
+            {
+                var para = new List<RequestParameter>
+                {
+                    new RequestParameter("IdDevice",idDevice),
+                    new RequestParameter("ContentVi",contentVi),
+                    new RequestParameter("ContentEn",contentEn),
+                    new RequestParameter("data1",data1),
+                    new RequestParameter("data2",data2),
+                    new RequestParameter("TitleVi",titleVi),
+                    new RequestParameter("TitleEn",titleEn),
+                };
+                var data = await _requestProvider.PostAsync<string>("PushNotifications/Send", para);
+                return data;
+            }
+            catch (Exception )
+            {
+                throw;
+            }
+        }
+    }
+}
