6c2a3298f972f752eb98ebbbad06f0ac26b8c8bf
kirapham <quanghuypham.95@gmail.com>
2020-12-25 17:07:27 +0700

    Calendar Page

--- KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/ViewModels/Pickup/PickupViewModel.cs
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/ViewModels/Pickup/PickupViewModel.cs
@@ -7,8 +7,12 @@ using KIDS.MOBILE.APP.PARENTS.Configurations;
 using KIDS.MOBILE.APP.PARENTS.Models.LeaveRequest;
 using KIDS.MOBILE.APP.PARENTS.Resources;
 using KIDS.MOBILE.APP.PARENTS.Services;
+using KIDS.MOBILE.APP.PARENTS.Views.Pickup;
+using Prism.Commands;
 using Prism.Navigation;
 using Xamarin.Forms;
+using Xamarin.Plugin.Calendar.Interfaces;
+using Xamarin.Plugin.Calendar.Models;
 
 namespace KIDS.MOBILE.APP.PARENTS.ViewModels.Pickup
 {
@@ -42,19 +46,53 @@ namespace KIDS.MOBILE.APP.PARENTS.ViewModels.Pickup
             set => SetProperty(ref _Late, value);
         }
 
+        private DateTime _SelectedDate;
+        public DateTime SelectedDate
+        {
+            get => _SelectedDate;
+            set => SetProperty(ref _SelectedDate, value);
+        }
+
+        private EventCollection _Events = new EventCollection();
+        public EventCollection Events
+        {
+            get => _Events;
+            set => SetProperty(ref _Events, value);
+        }
+
+        private int _Month;
+        public int Month
+        {
+            get => _Month;
+            set
+            {
+                _Month = value;
+                RaisePropertyChanged(nameof(Month));
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await GetAttendanceForMonth(new DateTime(SelectedDate.Year, Month, 1));
+                });
+            }
+        }
+
         private ILeaveRequestService _leaveRequestService;
 
         public PickupViewModel(INavigationService navigationService, ILeaveRequestService leaveRequestService) : base(navigationService)
         {
             _navigationService = navigationService;
             _leaveRequestService = leaveRequestService;
+            SelectedDate = DateTime.Now;
+            Month = DateTime.Now.Month;
         }
-
+        private void OnSwipeLeftClicked(object o)
+        {
+        }
         public override async void Initialize(INavigationParameters parameters)
         {
             base.Initialize(parameters);
             await GetAttendanceInformation();
             await GetListPickUpMessage();
+            //await GetAttendanceForMonth(SelectedDate);
         }
 
         private async Task GetAttendanceInformation()
@@ -76,14 +114,74 @@ namespace KIDS.MOBILE.APP.PARENTS.ViewModels.Pickup
 
         }
 
-        public async Task<List<GetAttendanceForMonthModel>> GetAttendanceForMonth(DateTime date)
+        public async Task GetAttendanceForMonth(DateTime date)
         {
             var startDate = new DateTime(date.Year, date.Month, 1);
             var data = await _leaveRequestService.GetAttendanceForMonth(AppConstants.User.ClassID.ToString(),
                 AppConstants.User.StudentID.ToString(),
                 startDate,
                 startDate.AddMonths(1).AddDays(-1));
-            return data?.Data;
+            if (data?.Data?.Any() == true)
+            {
+                var events = new EventCollection();
+                foreach (var dataItem in data.Data)
+                {
+                    Color color = dataItem.CoMat == true ? Color.Green : Color.Red;
+                    events.Add(
+                        dataItem.Date.Value,
+                        new DayEventCollection<object>{EventIndicatorColor = color, EventIndicatorSelectedColor = color });
+                }
+                Events = events;
+            }
         }
+    }
+
+    public class DayEventCollection<T> : List<T>, IPersonalizableDayEvent
+    {
+        /// <summary>
+        /// Empty contructor extends from base()
+        /// </summary>
+        public DayEventCollection() : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Color contructor extends from base()
+        /// </summary>
+        /// <param name="eventIndicatorColor"></param>
+        /// <param name="eventIndicatorSelectedColor"></param>
+        public DayEventCollection(Color? eventIndicatorColor, Color? eventIndicatorSelectedColor) : base()
+        {
+            EventIndicatorColor = eventIndicatorColor;
+            EventIndicatorSelectedColor = eventIndicatorSelectedColor;
+        }
+
+        /// <summary>
+        /// IEnumerable contructor extends from base(IEnumerable collection)
+        /// </summary>
+        /// <param name="collection"></param>
+        public DayEventCollection(IEnumerable<T> collection) : base(collection)
+        {
+
+        }
+
+        /// <summary>
+        /// Capacity contructor extends from base(int capacity)
+        /// </summary>
+        /// <param name="capacity"></param>
+        public DayEventCollection(int capacity) : base(capacity)
+        {
+
+        }
+
+        #region PersonalizableProperties
+        public Color? EventIndicatorColor { get; set; }
+        public Color? EventIndicatorSelectedColor { get; set; }
+        public Color? EventIndicatorTextColor { get; set; }
+        public Color? EventIndicatorSelectedTextColor { get; set; }
+
+        #endregion
+
     }
 }
--- KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/Views/Pickup/PickupPage.xaml.cs
+++ KIDS.MOBILE.APP.PARENTS/KIDS.MOBILE.APP.PARENTS/Views/Pickup/PickupPage.xaml.cs
@@ -17,85 +17,12 @@ namespace KIDS.MOBILE.APP.PARENTS.Views.Pickup
         {
             InitializeComponent();
         }
-        private List<GetAttendanceForMonthModel> Data = null;
+
         private PickupViewModel vm;
         protected override void OnAppearing()
         {
             base.OnAppearing();
             vm = (PickupViewModel)this.BindingContext;
-        }
-
-        private void Calendar_OnMonthCellLoaded(object sender, MonthCellLoadedEventArgs e)
-        {
-            if (Data == null) return;
-            var item = Data?.FirstOrDefault(x => x.Date?.Date == e.Date.Date);
-            var model = new AttendanceCalendarModel(e.Date,
-                item == null ? string.Empty : (item?.CoMat == true ? "done.png" : "error.png"));
-            e.CellBindingContext = model;
-        }
-
-        private async void Calendar_MonthChanged(object sender, MonthCellLoadedEventArgs e)
-        {
-            try
-            {
-                try
-                {
-                    loadingView.IsVisible = true;
-                    calendar.IsVisible = false;
-                    Data = await vm.GetAttendanceForMonth(e.Date);
-                    calendar.IsVisible = true;
-                    //calendar.Refresh();
-                }
-                finally
-                {
-                    loadingView.IsVisible = false;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-    }
-
-    public class AttendanceCalendarModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
-        {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private string image;
-        public string Image
-        {
-            get
-            {
-                return image;
-            }
-
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                {
-                    image = value;
-                    OnPropertyChanged(nameof(Image));
-                    OnPropertyChanged(nameof(IsVisible));
-                }
-            }
-        }
-
-        public bool IsVisible
-        {
-            get => !string.IsNullOrEmpty(Image);
-        }
-
-        public DateTime Date { get; set; }
-
-        public AttendanceCalendarModel(DateTime date, string image)
-        {
-            this.Date = date;
-            this.Image = image;
         }
     }
 }
